2e07d284d6d9383231dd7ff8c9c41b81d6c15fca
patiuma666 <patiuma666@gmail.com>
2017-11-07 13:57:11 +0530

    Add files via upload

---
+++ MainActivity.java
@@ -0,0 +1,26 @@
+package com.example.iis5.customlistview;
+
+import android.app.ListActivity;
+import android.content.Context;
+import android.content.res.Resources;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.TextView;
+
+//mainActivity is created
+public class MainActivity extends ListActivity {
+
+// this method called when MainActivty is created
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+        }
